f462ea7226f44c3aaf2d21fde546bda6c67a6152
jonon <jonathancjonsson@outlook.com>
2018-10-17 19:37:15 +0200

    Add files to create user

---
+++ src/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TwitterClone\Controllers;
+
+use TwitterClone\Exceptions\NotFoundException;
+
+class UserController extends AbstractController
+{
+    public function login(): string
+    {
+    }
+}
---
+++ src/Models/UserModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TwitterClone\Models;
+
+use TwitterClone\Domain\User;
+use TwitterClone\Exceptions\NotFoundException;
+use PDO;
+
+class UserModel extends AbstractModel
+{
+
+}
