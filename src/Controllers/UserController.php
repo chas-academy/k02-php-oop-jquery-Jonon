ea54fc5bf2f0bc4c1324ccb1b2fa9e1d182bd4cf
jonon <jonathancjonsson@outlook.com>
2018-10-22 21:52:11 +0200

    Add session login

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -3,10 +3,52 @@
 namespace TwitterClone\Controllers;
 
 use TwitterClone\Exceptions\NotFoundException;
+use TwitterClone\Models\UserModel;
 
 class UserController extends AbstractController
 {
     public function login(): string
     {
+        //  check if form is empty
+
+        //  checks if the request is a post request
+        if (!$this->request->isPost()) {
+            return $this->render('views/login.php');
+        }
+
+        //  get the parameters from the form
+        $params = $this->request->getParams();
+        var_dump($params);
+        
+        //  check if the parameters exist
+        if (!$params->has('email')) {
+            $params = ['errorMessage' => 'No info provided.'];
+            return $this->render('views/error.php', $params);
+        }
+
+        //
+        $email = $params->getString('email');
+
+        //  Validate email
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $params = ['errorMessage' => 'Email not valid.'];
+            return $this->render('views/error.php', $params);
+        }
+
+        //  Send to model
+        $userModel = new UserModel();
+       
+        try {
+            $user = $userModel->getByEmail($email);
+        } catch (NotFoundException $e) {
+            $params = ['errorMessage' => 'Email not found.'];
+            return $this->render('views/error.php', $params);
+        }
+        
+        $_SESSION['user'] = $user;
+        var_dump($_SESSION);
+
+        // header('Location: /');
+        return "test";
     }
 }
--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -8,5 +8,29 @@ use PDO;
 
 class UserModel extends AbstractModel
 {
+    const CLASSNAME = '\TwitterClone\Domain\User';
 
+    public function getByEmail(string $email): User
+    {
+        $query = 'SELECT * FROM users WHERE email = :email';
+        $sth = $this->db->prepare($query);
+
+        $params = [
+            'email' => $email
+        ];
+
+        
+        $sth->execute($params);
+
+        $row = $sth->fetchObject(self::CLASSNAME);
+        //return $row = $sth->fetch(self::CLASSNAME, 'User');
+        //return $row = $sth->fetch();
+        
+        
+        if (empty($row)) {
+            throw new NotFoundException();
+        }
+
+        return $row;
+    }
 }
