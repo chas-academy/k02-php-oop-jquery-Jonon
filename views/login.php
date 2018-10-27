7aee5ba76b6974d9f05710cc6929d6bd56b62394
jonon <jonathancjonsson@outlook.com>
2018-10-27 10:21:57 +0200

    Add check if session is set adn links for login and regisster
    Check if session is set. If set show only logout button and if not show login and register buttons

--- views/login.php
+++ views/login.php
@@ -1,3 +1,24 @@
-<?php
-
-echo "hej!";
+<div class="container py-5">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6 mx-auto">
+                <form action="/login" method="post">
+                        <h1 class="h3 mb-3 font-weight-normal text-center">Please login</h1>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control" name="email" placeholder="Email" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" name="password" placeholder="Password" required>
+                        </div>
+                        <div class="form-group d-flex justify-content-end">
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
