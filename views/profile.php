57e5efb3ebaa780de2a9584400e82431f0d46c6c
jonon <jonathancjonsson@outlook.com>
2018-10-30 21:35:44 +0100

    Add profile view

---
+++ views/profile.php
@@ -0,0 +1,10 @@
+<div class="jumbotron default__background-image">
+
+</div>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">name<?php
+    ?></h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
