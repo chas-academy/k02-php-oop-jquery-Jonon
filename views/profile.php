07f082890fb0644edc1609cb57190983fbdf212d
jonon <jonathancjonsson@outlook.com>
2018-10-31 10:45:25 +0100

    Add profile view

---
+++ views/profile.php
@@ -0,0 +1,10 @@
+<div class="default__background-image"></div>
+<div class="container-fluid">
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Name</h5>
+        <h6 class="card-subtitle mb-2 text-muted">username</h6>
+        <p class="card-text">some text about myself.</p>
+    </div>
+    </div>
+</div>
