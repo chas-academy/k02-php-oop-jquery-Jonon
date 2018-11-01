d0f8bec5817c53509e9c8feeb46a4afdbe5e584d
jonon <jonathancjonsson@outlook.com>
2018-11-01 20:57:58 +0100

    Add dropdown on dropdown__icon-button

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+//on profile icon button dropdown option
+
+$(document).ready(() => {
+    $('.dropdown__icon-button').on('click', () => {
+        $('.dropdown-active').toggle()
+    })
+
+    $('.dropdown-active').on('mouseleave', () => {
+        $('.dropdown-active').hide()
+    })
+
+
+})
+
