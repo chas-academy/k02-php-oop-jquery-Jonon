36ac7a3e3bfa444c5617af2162dce847ec2b155d
jonon <jonathancjonsson@outlook.com>
2018-11-05 21:57:55 +0100

    Change tweet layout

--- views/profile.php
+++ views/profile.php
@@ -3,13 +3,29 @@
     </div>
 </div>
 
-<nav class="navbar navbar-light bg-light border_bottom box-shadow-b navbar-height">
+<nav class="navbar navbar-light bg-light border_bottom box-shadow-b">
+    <div class=" d-flex justify-content-end">
+        
+        <ul class="navbar-nav flex-row w-50 ">
+            <li class="navbar-item ml-2">
+                <a href="#">Tweets</a>
+            </li>
+            <li class="navbar-item ml-2">
+                <a href="#">Following</a>
+            </li>
+            <li class="navbar-item ml-2">
+                <a href="#">Followers</a>
+            </li>
+        </ul>
+        <a href="#" class="btn btn-outline-primary">Edit Profile</a>
+    </div>     
 </nav>
+
 <div class="container">
     <div class="row">
         <div class="col-sm-4 mt-5-invert ">
             <div class="profile profile--large img-thumbnail rounded-circle"></div>
-            <div class="card mt-3">
+            <div class="info-card mt-3 ml-5">
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $user->getName();?></h5>
                     <h6 class="card-subtitle mb-2 text-muted"><?php echo $user->getUsername();?></h6>
@@ -22,20 +38,16 @@
         <div class="col-sm-6 mt-3">
             <?php
             for ($i = 0; $i <= 5; $i++) : ?>
-                <div class="card">
-                    <div class="card-body mt-2">
-                        <div class="row">
-                            <div class="profile profile--small rounded-circle"></div>
-                            <div class="ml-3">
-                                <h6 class="card-title"><?php echo $user->getName();?></h6>
-                                <p class="card-text">some tweet text.</p>
-                                <nav>
-                                <i class="far fa-comment"></i>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
+            <div class="media border border-primary">
+                <img class="align-self-start mr-3 rounded-circle profile--small" src="../images/profile_default.jpg" alt="Generic placeholder image">
+                <div class="media-body">
+                    <h5 class="mt-0"><?php echo $user->getName();?></h5>
+                    <p>It’s the ship that made the Kessel Run in less than twelve parsecs! I’ve outrun Imperial starships, not the local bulk-cruisers, mind you. I’m talking about the big Corellian ships now. She’s fast enough for you, old man.</p>
+                    <nav>
+                        <i class="far fa-comment"></i>
+                    </nav>
                 </div>
+            </div>
             <?php endfor; ?>
         </div>
     </div>
