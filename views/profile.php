8e23b67be304b08507306b5d9be7247562b55c29
jonon <jonathancjonsson@outlook.com>
2018-11-06 22:47:03 +0100

    Add like icon and align menu links and button

--- views/profile.php
+++ views/profile.php
@@ -3,10 +3,10 @@
     </div>
 </div>
 
-<nav class="navbar navbar-light bg-light border_bottom box-shadow-b">
-    <div class=" d-flex justify-content-end">
-        
-        <ul class="navbar-nav flex-row w-50 ">
+<nav class="navbar navbar-light bg-light border_bottom box-shadow-b ">
+    <div class="d-flex justify-content-end">
+        <div class="d-flex w-50 justify-content-sm-between">
+        <ul class="navbar-nav flex-row">
             <li class="navbar-item ml-2">
                 <a href="#">Tweets</a>
             </li>
@@ -17,16 +17,22 @@
                 <a href="#">Followers</a>
             </li>
         </ul>
-        <a href="#" class="btn btn-outline-primary">Edit Profile</a>
-    </div>     
+        <div class="">
+        <a href="#" class="btn btn-outline-primary ">Edit Profile</a>
+        </div>
+        
+        </div> 
+        </div>
+
+        
 </nav>
 
 <div class="container">
     <div class="row">
-        <div class="col-sm-4 mt-5-invert ">
-            <div class="profile profile--large img-thumbnail rounded-circle"></div>
+        <div class="col-sm-4">
             <div class="info-card mt-3 ml-5">
                 <div class="card-body">
+                    <img class="align-self-start mr-3 rounded-circle profile--large" src="../images/profile_default.jpg" alt="Profile avatar">
                     <h5 class="card-title"><?php echo $user->getName();?></h5>
                     <h6 class="card-subtitle mb-2 text-muted"><?php echo $user->getUsername();?></h6>
                     <p class="card-text">some text about myself.</p>
@@ -39,12 +45,13 @@
             <?php
             for ($i = 0; $i <= 5; $i++) : ?>
             <div class="media border border-primary">
-                <img class="align-self-start mr-3 rounded-circle profile--small" src="../images/profile_default.jpg" alt="Generic placeholder image">
+                <img class="align-self-start mr-3 rounded-circle profile--small" src="../images/profile_default.jpg" alt="profile avatar">
                 <div class="media-body">
                     <h5 class="mt-0"><?php echo $user->getName();?></h5>
                     <p>It’s the ship that made the Kessel Run in less than twelve parsecs! I’ve outrun Imperial starships, not the local bulk-cruisers, mind you. I’m talking about the big Corellian ships now. She’s fast enough for you, old man.</p>
                     <nav>
                         <i class="far fa-comment"></i>
+                        <i class="far fa-heart"></i>
                     </nav>
                 </div>
             </div>
