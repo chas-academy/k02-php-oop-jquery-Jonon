efc9a827d502e6cd500639388a0468a2a38c66f7
jonon <jonathancjonsson@outlook.com>
2018-11-09 20:17:15 +0100

    Add new content to front page

--- views/layout.php
+++ views/layout.php
@@ -1,6 +1,15 @@
-<section class="jumbotron text-center">
+<section class="jumbotron jumbo-home text-center">
     <div class="container">
         <h1 class="jumbotron-heading">Välkommen till TwitterClone!</h1>
-        <p class="lead text-muted">Här skriver vi också något om vår TwitterClone. Samt har alltid minst en knapp som blir call-to-action</p>
+        <p class="lead text-muted">Join TwitterClone today to follow your friends and take part of interssting conversations</p>
+        <a class ="btn btn-info" href="/register">Register</a>
     </div>
 </section>
+<div class="container">
+    <div class="row align-items-center text-warning">
+        <h2 class="col ">Follow Your Interests</h2>
+        <h2 class="col">Join conversations</h2>
+        <h2 class="col">See what people are talking about</h2>
+    </div>
+    <p class="text-center">Have a sneakpeak, click here</p>
+</div>
