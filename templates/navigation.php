dffb0fff8fb0623d9ea7b2ea3acaf37acd9534e3
jonon <jonathancjonsson@outlook.com>
2018-11-13 19:52:14 +0100

    Add fixed position to navigation

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-light bg-light border_bottom p-x-sm-0 box-shadow-b">
+<nav class="navbar navbar-light bg-light border_bottom p-x-sm-0 box-shadow-b fixed-top">
     <div class="d-sm-flex justify-content-between">     
         <div class="d-flex w-100 justify-content-around">
         <?php
