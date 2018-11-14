a0a299a7978b8be1651c41afabbe43816de74f2c
jonon <jonathancjonsson@outlook.com>
2018-11-14 13:20:14 +0100

    Add border-radius on not logged in view

--- templates/navigation.php
+++ templates/navigation.php
@@ -58,7 +58,7 @@ $html = <<<HTML
             <div class="dropdown">
             <!-- profile icon-->
             <div class="d-flex align-items-center">
-            <i data-feather="user" class=" dropdown__icon-button"></i>
+            <i data-feather="user" class=" dropdown__icon-button rounded-circle"></i>
                 <i class="arrow-down"></i>
             </div>
                 
