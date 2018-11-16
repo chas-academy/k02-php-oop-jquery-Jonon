b8000a51e8d59ab63d701d43cce71ac54ec35e61
jonon <jonathancjonsson@outlook.com>
2018-11-16 12:38:20 +0100

    Add form for creating tweets

---
+++ views/home.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <form>
+        <div class="form-group row justify-content-center">
+            <textarea class="textarea-resize-none text-center-placeholder textarea_tweet" placeholder="What's happening?"></textarea>
+            <div class="ml-5">
+                <button class="btn btn-primary" type="text">Tweet</button>
+            </div>        
+        </div>
+    </form>
+</div>
