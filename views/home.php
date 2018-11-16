0f3e871c0e6c32323285a1802fe7b099cf374bd8
jonon <jonathancjonsson@outlook.com>
2018-11-16 14:18:45 +0100

    Change styling and add a method and a action for the form

--- views/home.php
+++ views/home.php
@@ -1,9 +1,9 @@
 <div class="container">
-    <form>
+    <form action="/home" method="post">
         <div class="form-group row justify-content-center">
-            <textarea class="textarea-resize-none text-center-placeholder textarea_tweet" placeholder="What's happening?"></textarea>
+            <textarea class="textarea-resize-none text-center-placeholder textarea_tweet" placeholder="What's happening?" name="tweet"></textarea>
             <div class="ml-5">
-                <button class="btn btn-primary" type="text">Tweet</button>
+                <button class="btn btn-primary" type="submit">Tweet</button>
             </div>        
         </div>
     </form>
