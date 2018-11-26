b9eaf2a66aea957dd9250af17560aaeaab674847
jonon <jonathancjonsson@outlook.com>
2018-11-26 14:46:20 +0100

    Add delete function for controller

---
+++ src/Controllers/TweetController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TwitterClone\Controllers;
+
+use TwitterClone\Exceptions\NotFoundException;
+use TwitterClone\Models\UserModel;
+use TwitterClone\Models\TweetModel;
+
+class TweetController extends AbstractController
+{
+    public function delete()
+    {
+        
+    }
+}
