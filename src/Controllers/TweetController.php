b96ba8d5ed5c28f711d2d438c76c19f2286fa725
jonon <jonathancjonsson@outlook.com>
2018-11-27 15:25:40 +0100

    get tweetId from form and delete tweet from table tweets

--- src/Controllers/TweetController.php
+++ src/Controllers/TweetController.php
@@ -10,6 +10,17 @@ class TweetController extends AbstractController
 {
     public function delete()
     {
-        
+        $params = $this->request->getParams();
+        $tweetModel = new TweetModel();
+
+        $properties =
+        [
+            'userId' => $params->get('Id'),
+            'tweetId' => $params->get('tweetId')
+        ];
+
+        $deleteTweet = $tweetModel->deleteTweet($properties);
+
+        header("location: /profile/". $this->getAuthenticatedUser());
     }
 }
