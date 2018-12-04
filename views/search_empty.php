19673ec083fefcd01f5dcfcb1a1d384f0b4cc02a
jonon <jonathancjonsson@outlook.com>
2018-12-04 13:53:00 +0100

    if form is empty return this view

---
+++ views/search_empty.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <form class="form-row d-flex justify-content-center mt-3">
+        <div class="row col-auto">
+            <div class="form-group mb-2 col-auto">
+                <input type="text" class="form-control" name="search" placeholder="Search">
+            </div>
+            <div class="col-auto form-group">
+                <button type="submit" class="btn btn-primary mb-2 form-control ">Search</button>
+            </div>
+        </div> 
+    </form>
+</div>
