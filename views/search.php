7044ca9fde1e8e8eb7794cceaf8a6474aa748910
jonon <jonathancjonsson@outlook.com>
2018-12-04 13:52:28 +0100

    Add search result on success

--- views/search.php
+++ views/search.php
@@ -8,5 +8,36 @@
                 <button type="submit" class="btn btn-primary mb-2 form-control ">Search</button>
             </div>
         </div> 
-    </form> 
+    </form>
+    <div class="row">
+    <?php foreach ($users as $user) :?>
+    <div class="card-group col-sm-4 mb-4">
+    <a href="/profile/<?php echo $user->getUsername()?>">
+        <!-- Card -->
+        <div class="card col testimonial-card">
+        <!-- Background color -->
+        <div class="card-up indigo lighten-1"></div>
+        <!-- Avatar -->
+        <div class="avatar mx-auto white mt-1">
+            <img src="https://images.unsplash.com/photo-1542384701-c0e46e0eda04?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1339324b1b254b13747a87333861327f&auto=format&fit=crop&w=636&q=80" class="rounded-circle profile--large" alt="woman avatar">
+        </div>
+        <!-- Content -->
+        <div class="card-body">
+            <!-- Name -->
+            <h4 class="card-title"><?php echo $user->getName()?></h4>
+            <hr>
+            <!-- Quotation -->
+            <p><?php echo $user->getDescription()?></p>
+        </div>
+        </div>
+<!-- Card -->
+</a>
+    </div>
+    <?php endforeach?>
+    </div>
 </div>
+
+
+
+
+
