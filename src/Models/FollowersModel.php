70f5785d613ff52e732943b40dcd32db5e7cb089
jonon <jonathancjonsson@outlook.com>
2018-12-09 17:16:39 +0100

    Add new model for followers

---
+++ src/Models/FollowersModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TwitterClone\Models;
+
+use TwitterClone\Domain\Follower;
+use TwitterClone\Exceptions\NotFoundException;
+use PDO;
+
+class FollowersModel extends AbstractModel
+{
+
+}
