9c8a5cb30ef5052f95171d0e75bce5f955cc7a7f
jonon <jonathancjonsson@outlook.com>
2018-12-12 22:03:00 +0100

    Delete getusersFolllwing and add function for checking if logged in user is following profile user

--- src/Models/FollowerModel.php
+++ src/Models/FollowerModel.php
@@ -10,28 +10,6 @@ class FollowerModel extends AbstractModel
 {
     const CLASSNAME = '\TwitterClone\Domain\Follower';
 
-    public function getUsersFollowing($profileUserId)
-    {
-        $query = 'SELECT * FROM followers
-        WHERE followerId = :followerId';
-
-        $sth = $this->db->prepare($query);
-
-        $params = [
-            'followerId' => $profileUserId
-        ];
-
-        $sth->execute($params);
-
-        $followers = $sth->fetchAll(PDO::FETCH_CLASS, self::CLASSNAME);
-
-        if (empty($followers)) {
-            throw new NotFoundException();
-        }
-
-        return $followers;
-    }
-
     public function getIfFollowing($params)
     {
         $query = 'SELECT * FROM followers 
