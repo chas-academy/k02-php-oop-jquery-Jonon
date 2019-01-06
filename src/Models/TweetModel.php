5889fc33fd7b5203977bd3067881d74509894345
jonon <jonathancjonsson@outlook.com>
2019-01-06 17:53:46 +0100

    Get all tweets from users following

--- src/Models/TweetModel.php
+++ src/Models/TweetModel.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TwitterClone\Models;
 
 use TwitterClone\Domain\Tweet;
@@ -9,8 +11,11 @@ use PDO;
 class TweetModel extends AbstractModel
 {
     const CLASSNAME = '\TwitterClone\Domain\Tweet';
+    const DTOCLASSNAME = '\TwitterClone\Domain\DtoTweetFromFollowedUser';
 
-    public function getTweets($username)
+    
+
+    public function getTweets(string $username): array
     {
         $query = 'SELECT * FROM users
         INNER JOIN tweets
@@ -31,7 +36,7 @@ class TweetModel extends AbstractModel
         return $tweets;
     }
 
-    public function createTweet($properties)
+    public function createTweet(array $properties): void
     {
         $query = 'INSERT INTO tweets (tweet, id)
         VALUES(:tweet, :id)';
@@ -44,7 +49,7 @@ class TweetModel extends AbstractModel
         $sth->execute();
     }
 
-    public function deleteTweet($properties)
+    public function deleteTweet(array $properties): void
     {
         $query = 'DELETE FROM tweets
         WHERE id = :userId AND tweetId = :tweetId';
@@ -54,5 +59,22 @@ class TweetModel extends AbstractModel
         $sth->bindValue(':tweetId', $properties['tweetId']);
         
         $sth->execute();
+    }
+
+    public function getTweetsByUsersFollowing(int $userId): array
+    {
+        $query = 'SELECT * FROM users
+        INNER JOIN tweets
+        ON users.id = tweets.id
+        INNER JOIN followers
+        ON users.id = followers.userId
+        WHERE followers.followerId = :followerId';
+
+        $sth = $this->db->prepare($query);
+        $sth->bindValue(':followerId', $userId);
+
+        $sth->execute();
+
+        return $sth->fetchAll(PDO::FETCH_CLASS, self::DTOCLASSNAME);
     }
 }
