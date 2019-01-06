5fc6f6079436c02664e61e0221d64b6e7b35e0f7
jonon <jonathancjonsson@outlook.com>
2019-01-06 17:49:57 +0100

    New view for home

--- views/home.php
+++ views/home.php
@@ -7,4 +7,20 @@
             </div>        
         </div>
     </form>
-</div>
+
+
+    <?php
+    foreach ($tweets as $tweet) :?>
+        <div class="media border border-primary box-shadow-b mt-2 p-2 ">
+            <img class="align-self-start mr-3 rounded-circle profile--small" src="https://images.unsplash.com/photo-1542384701-c0e46e0eda04?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1339324b1b254b13747a87333861327f&auto=format&fit=crop&w=636&q=80" alt="profile avatar">
+            <div class="media-body">
+                <h5 class="mt-0"><?php echo $tweet->getName();?></h5>
+
+                <p><?php  echo $tweet->getTweet();?></p>
+                <nav class="">
+                    <i class="feather f-message-circle" data-feather="message-circle"></i>
+                    <i class="feather f-heart ml-5" data-feather="heart"></i>
+                </nav>
+            </div>
+        </div>
+    <?php endforeach; ?>
