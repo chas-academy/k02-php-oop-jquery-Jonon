9aa3a4a3453f68da6a67d622634bbbbadbc71590
jonon <jonathancjonsson@outlook.com>
2019-01-07 22:37:33 +0100

    Center tweets

--- views/home.php
+++ views/home.php
@@ -11,16 +11,20 @@
 
     <?php
     foreach ($tweets as $tweet) :?>
-        <div class="media border border-primary box-shadow-b mt-2 p-2 ">
-            <img class="align-self-start mr-3 rounded-circle profile--small" src="https://api.adorable.io/avatars/285/jons.png" alt="profile avatar">
-            <div class="media-body">
-                <h5 class="mt-0"><?php echo $tweet->getName();?></h5>
-
-                <p><?php  echo $tweet->getTweet();?></p>
-                <nav class="">
-                    <i class="feather f-message-circle" data-feather="message-circle"></i>
-                    <i class="feather f-heart ml-5" data-feather="heart"></i>
-                </nav>
+    <div class="row justify-content-center">
+        <div class="col-sm-8">
+            <div class="media border border-primary box-shadow-b mt-2 p-2">
+                <img class="align-self-start mr-3 rounded-circle profile--small" src="https://api.adorable.io/avatars/285/jons.png" alt="profile avatar">
+                <div class="media-body">
+                    <h5 class="mt-0"><?php echo $tweet->getName();?></h5>
+                    <p><?php  echo $tweet->getTweet();?></p>
+                    <nav class="">
+                        <i class="feather f-message-circle" data-feather="message-circle"></i>
+                        <i class="feather f-heart ml-5" data-feather="heart"></i>
+                    </nav>
+                </div>
             </div>
         </div>
+    </div>
     <?php endforeach; ?>
+</div>
