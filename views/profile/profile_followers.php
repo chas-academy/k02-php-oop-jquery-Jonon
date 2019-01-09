9fc7d739c162923116414a0a32d802b651bb0970
jonon <jonathancjonsson@outlook.com>
2019-01-09 13:31:27 +0100

    Fix column width

--- views/profile/profile_followers.php
+++ views/profile/profile_followers.php
@@ -1,1 +1,19 @@
-<?php
+<div class="container">
+    <div class="row mt-2">
+    <?php foreach ($followers as $follows) :?>
+        <div class="col-sm-4">
+            <a class="disable-a" href="/profile/<?php echo $follows->getUsername()?>">
+                <div class="card">
+                    <img class="card-img-top" src="https://picsum.photos/300/100/?random" alt="Card image cap">
+                    <img class="d-flex ml-3 rounded-circle profile--medium mt-1-invert" src="https://api.adorable.io/avatars/285/jons.png" alt="Generic placeholder image">
+                    <div class="card-block card-block-height">
+                        <h4 class="card-title"><?php echo $follows->getName()?></h4>
+                        <h6 class="card-title"><?php echo '@' . $follows->getUsername()?></h6>
+                        <p class="card-text"><?php echo $follows->getDescription()?></p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    <?php endforeach; ?>
+    </div>
+</div>
