8c8a83e067221265c55ff4333f0af882554bf578
jonon <jonathancjonsson@outlook.com>
2019-01-10 15:39:43 +0100

    Add layout styling

--- views/layout.php
+++ views/layout.php
@@ -1,15 +1,14 @@
 <section class="jumbotron jumbo-home text-center">
     <div class="container">
-        <h1 class="jumbotron-heading">Välkommen till TwitterClone!</h1>
-        <p class="lead text-muted">Join TwitterClone today to follow your friends and take part of interssting conversations</p>
+        <h1 class="jumbotron-heading font-oswald">Welcome to TwitterClone!</h1>
+        <p class="lead text-muted font-oswald">Join TwitterClone today to follow your friends and take part of interssting conversations</p>
         <a class ="btn btn-info" href="/register">Register</a>
     </div>
 </section>
 <div class="container">
-    <div class="row align-items-center text-warning">
-        <h2 class="col ">Follow Your Interests</h2>
-        <h2 class="col">Join conversations</h2>
-        <h2 class="col">See what people are talking about</h2>
+    <div class="row align-items-center">
+        <h2 class="col text-pink">Follow Your Interests</h2>
+        <h2 class="col text-pink">Join conversations</h2>
+        <h2 class="col text-pink">See what people are talking about</h2>
     </div>
-    <p class="text-center">Have a sneakpeak, click here</p>
 </div>
