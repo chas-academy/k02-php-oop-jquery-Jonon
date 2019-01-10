bbad2cb3f3bc74fb6a4d4f291d46e2e5c8a6addf
jonon <jonathancjonsson@outlook.com>
2019-01-10 15:24:00 +0100

    View edited

--- views/profile/profile_profile-user.php
+++ views/profile/profile_profile-user.php
@@ -2,43 +2,43 @@
     <div id="jumbo" class="jumbotron default__background-image">
     </div>
 </div>
-<nav class="navbar navbar-light bg-light border_bottom box-shadow-b">
-    <div class="d-flex justify-content-center ">
-        <div class="d-flex  aligner flex-wrap">
-            <ul class="navbar-nav flex-row">
-                <li class="navbar-item ml-5">
-                    <a href="#">Tweets</a>
-                </li>
-                <li class="navbar-item ml-5">
-                    <a href="/profile/<?php echo $user->getusername()?>/following">Following</a>
-                </li>
-                <li class="navbar-item ml-5">
-                    <a href="/profile/<?php echo $user->getusername()?>/followers">Followers</a>
-                </li>
-            </ul>
-            <button type="button" class="btn btn-outline-primary ml-5 d-sm-none d-md-block  d-sm-block d-none" data-toggle="modal" data-target="#ModalCenter">Edit Profile
-            </button>
-        </div> 
+<nav class="navbar-secondary navbar-light bg-light border_bottom box-shadow-b d-sm-none d-md-block d-sm-block d-none">
+    <div class="d-flex justify-content-end ">
+        <div class="mr-5">
+            <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#ModalCenter">Edit Profile
+        </button>
+        </div>
     </div>
 </nav>
 
 
 <div class="container">
     <div class="row">
-        <div class="col-md-4">
-            <div class="info-card mt-3 ml-5">
-                    <div class="card-body">
-                        <img class="align-self-start mr-3 rounded-circle profile--large" src="https://api.adorable.io/avatars/285/jons.png" alt="Profile avatar">
-                        <h5 class="card-title"><?php echo $user->getName();?></h5>
-                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $user->getUsername();?></h6>
-                        <p class="card-text"><?php echo $user->getDescription()?></p>
-                        <p class="card-text">Joined <?php echo $user->getDateJoined()?></p>
+        <div class="col-md-4 padding-nullify">
+            <div class="info-card mt-3 ">
+                <div class="card-body profile-card ">
+                    <div class="d-flex justify-content-between">
+                        <img class="align-self-start mr-3 rounded-circle profile--primary-size" src="https://api.adorable.io/avatars/285/jons.png" alt="Profile avatar">
+                        <button type="button" class="btn btn--mobile btn-outline-primary profile-card_btn d-block d-sm-none" data-toggle="modal" data-target="#ModalCenter">Edit Profile</button>
                     </div>
+                    <h5 class="card-title mt-2"><?php echo $user->getName();?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $user->getUsername();?></h6>
+                    <p class="card-text"><?php echo $user->getDescription()?></p>
+                    <p class="card-text">Joined <?php echo $user->getDateJoined()?></p>
+
+                    <ul class="d-flex p-0">
+                        <li class="navbar-item list-style">
+                            <a class="link-color-primary" href="/profile/<?php echo $user->getusername()?>/following">Following</a>
+                        </li>
+                        <li class="navbar-item list-style ml-5 pb-4">
+                            <a class="link-color-primary" href="/profile/<?php echo $user->getusername()?>/followers">Followers</a>
+                        </li>
+                    </ul>
                 </div>
+            </div>
         </div>
 
-
-        <div class="col-md-6">
+        <div class="col-md-6 padding-nullify">
         <?php
         foreach ($tweets as $tweet) :?>
             <div class="media border border-primary box-shadow-b mt-2 p-2 ">
