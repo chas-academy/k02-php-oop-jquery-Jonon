05a7a9b7a15a9862b2404d660dc0a90f666b97bd
jonon <jonathancjonsson@outlook.com>
2019-01-13 19:31:29 +0100

    Add view on following and followers on small screen devices

--- views/profile/profile_following.php
+++ views/profile/profile_following.php
@@ -6,13 +6,13 @@
     </a>
     <div class="row mt-2">
     <?php foreach ($following as $follows) :?>
-        <div class="col-sm-4">
+        <div class="col-sm-4 d-none d-sm-block">
             <a class="disable-a" href="/profile/<?php echo $follows->getUsername()?>">
                 <div class="card">
                     <img class="card-img-top" src="https://picsum.photos/300/100/?random" alt="Card image cap">
                     <img class="d-flex ml-3 rounded-circle profile--medium mt-1-invert" src="https://api.adorable.io/avatars/285/jons.png" alt="Generic placeholder image">
                     <div class="card-block card-block-height">
-                        <h4 class="card-title"><?php echo $follows->getName()?></h4>
+                        <h5 class="card-title"><?php echo $follows->getName()?></h5>
                         <h6 class="card-title"><?php echo '@' . $follows->getUsername()?></h6>
                         <p class="card-text"><?php echo $follows->getDescription()?></p>
                     </div>
@@ -22,3 +22,21 @@
     <?php endforeach; ?>
     </div>
 </div>
+
+    <div class="d-block d-sm-none">
+    <?php
+    foreach ($following as $follows) :?>
+    <a class="disable-a" href="/profile/<?php echo $follows->getUsername()?>">
+        <div class="pl-2 media user-card user-card--shadow mt-2">
+            <img class="d-flex mr-3 rounded-circle profile--small" src="https://api.adorable.io/avatars/285/jons.png" alt="Generic placeholder image">
+            <div class="media-body user-card__body">
+                <h5 class="card-title"><?php echo $follows->getName()?></h5>
+                <h6 class="card-title"><?php echo '@' . $follows->getUsername()?></h6>
+                <div class="user-card__body-text">
+                    <p class="card-text "><?php echo $follows->getDescription()?></p>
+                </div>
+            </div>
+        </div>
+    </a>
+    <?php endforeach; ?>
+    </div>
