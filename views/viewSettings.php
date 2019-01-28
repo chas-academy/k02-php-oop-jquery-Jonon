eae61a28fb8b37f580c17c3501829d30b528c75e
jonon <jonathancjonsson@outlook.com>
2019-01-28 15:34:16 +0100

    Add settings view

---
+++ views/viewSettings.php
@@ -0,0 +1,31 @@
+<div class="container ">
+    <div class="row justify-content-center align-items-center settings">
+        <div class="col-8 ">
+            <h1 class="text-center">Settings</h1>
+            <form>
+                <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" placeholder="Username">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" placeholder="E-mail">
+                    </div>
+                </div>
+                <div class="d-flex justify-content-end">
+                    <button class="btn btn-primary">Save settings</button>
+                </div>
+                
+            </form>
+
+            <h2 class="text-center">Delete account</h2>
+            <p class="text-center">Warning, clicking this button will delete your account permanently!</p>
+            <div class="d-flex justify-content-center">
+                <button class="btn btn-primary">Delete your account</button>
+            </div>
+        </div>
+    </div>
+</div>
